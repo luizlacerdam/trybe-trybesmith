6c9143e72247c3c15dbc0276d9e02c6a63d76dbc
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-03-14 20:50:18 -0300

    :sparkles: [feat] createAllUsers-route

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import productRoute from './routes/productRoute';
+import userRoute from './routes/userRoute';
 
 const app = express();
 
 app.use(express.json());
 app.use('/products', productRoute);
+app.use('/users', userRoute);
+
 export default app;
