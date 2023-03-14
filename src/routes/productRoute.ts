0929ea8c51fcb3b6c897d4b1b90e8a7c1bc73950
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-03-14 20:00:27 -0300

    :sparkles: [feat] req-02

--- src/routes/productRoute.ts
+++ src/routes/productRoute.ts
@@ -4,5 +4,6 @@ import * as productController from '../controllers/productController';
 const router = Router();
 
 router.post('/', productController.createProduct);
+router.get('/', productController.getAllProducts);
 
 export default router;
