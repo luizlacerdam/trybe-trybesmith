ce0358b61ce502a1d100b95db2b69912b21e7635
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-03-14 20:34:49 -0300

    :sparkles: [feat] createUser-model

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,9 +1,15 @@
 import { ResultSetHeader } from 'mysql2';
 import connection from './connection';
-import { NewUser, IUser } from '../interfaces/userInterface';
+import { NewUser } from '../interfaces/userInterface';
 
-const createUser = async (newUser: NewUser): Promise<NewUser> => {
-
+const createUser = async (newUser: NewUser) => {
+  const { username, vocation, level, password } = newUser;
+  const [result] = await connection.execute<ResultSetHeader>(
+    'INSERT INTO Trybesmith.users (username, vocation, level, password) VALUES (?,?,?,?)',
+    [username, vocation, level, password],
+  );
+  const { insertId: id } = result;
+  return { id, ...result };
 };
 
 const getAllUsers = async () => {
