ec44e0db7836963321db407f52178cf6bded651e
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-03-14 18:56:06 -0300

    :sparkles: [feat] productController

---
+++ src/controllers/productController.ts
@@ -0,0 +1,15 @@
+import { Request, Response } from 'express';
+import * as productService from '../services/productService';
+
+const createProduct = async (req: Request, res: Response) => {
+  const newProduct = req.body;
+  const { status, data } = await productService.createProduct(newProduct);
+  return res.status(status).json(data);
+};
+
+const getAllProducts = async () => {};
+
+export {
+  createProduct,
+  getAllProducts,
+};
