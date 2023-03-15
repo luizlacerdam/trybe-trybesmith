57d066263e9233d232a3a7905a7e33114392b57e
luizlacerdam <luizlacerdam.dev@gmail.com>
2023-03-15 14:52:52 -0300

    :sparkles: [feat] orderModel

--- src/models/orderModel.ts
+++ src/models/orderModel.ts
@@ -1,2 +1,20 @@
-import { ResultSetHeader } from 'mysql2';
+import IOrder from '../interfaces/orderInterface';
 import connection from './connection';
+
+const getOrderwithProductsIds = async ():Promise<IOrder[]> => {
+  const [result] = await connection.execute(
+    `SELECT o.id, o.user_id AS userId, JSON_ARRAYAGG(p.id) AS productsIds 
+    FROM Trybesmith.orders AS o 
+    INNER JOIN Trybesmith.products AS p ON o.id = p.order_id;`,
+  );
+  return result as IOrder[];
+};
+
+const get = async () => {
+
+};
+
+export {
+  get,
+  getOrderwithProductsIds,
+};
